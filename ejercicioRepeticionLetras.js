695c2dd4036508eddf1925522e9f4af1488f6df6
Fran Grau <grau.fran@gmail.com>
2018-10-29 10:55:53 +0100

    Simplificación ejercicio repetición letras

--- ejercicioRepeticionLetras.js
+++ ejercicioRepeticionLetras.js
@@ -10,15 +10,7 @@ function letrasUnicas (arrLetras) {
     dicLetras[letra]++ // Siempre incrementamos el contador para cada letra
   }
   var arrLetrasUnicas = Object.keys(dicLetras) // Obtenemos solo las claves/propiedades del dicionario, es decir las letras
-  arrLetrasUnicas.sort(function (letra1, letra2) { // Las ordenamos
-    if (letra1 < letra2) { // Si la letra 1 es menor que 2 devolvemos -1
-      return -1
-    }
-    if (letra2 > letra1) { // Si la letra 1 es mayor que la 2, 1
-      return 1
-    }
-    return 0 // Devolvemos 0 si son la misma (no debería ocurrir nunca)
-  })
+  arrLetrasUnicas.sort() // Ordenación simple del array (usando los valores UNICODE)
   var result = [] // Nos creamos un array para el resultado
   for (indiceLetra in arrLetrasUnicas) { // Recorremos el nuevo array de letras ordenadas
     letra = arrLetrasUnicas[indiceLetra] // Leemos la letra actual de la iteración
