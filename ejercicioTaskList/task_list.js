9e698afaa1097ab1594b284880c6d5a99f33d50e
Fran Grau <grau.fran@gmail.com>
2018-11-09 17:07:53 +0100

    Actualización TODOs

--- ejercicioTaskList/task_list.js
+++ ejercicioTaskList/task_list.js
@@ -7,3 +7,16 @@ function initTaskList () {
     window.location = 'index.html'
   }
 }
+
+// TODO Obtener la lista de usuarios y mostrar los activos.
+// TODO Obtener la lista de tareas y mostrar las tareas por completar y en otra lista las completadas:
+//  - Recuperar las tareas cada x segundos.
+//  - Mostrar de otra forma las tareas que están siendo editadas por otros usuarios (y deshabilitar la edición.
+// TODO Permitir crear nuevas tareas.
+// TODO Permitir editar tarea:
+//  - Al editar hacer petición PUT cambiado el valor de editor_id al usuario actual. PUT /api/tasks/[id]/
+//  - Al editar una tarea mostrar opciones:guardar, cancelar o eliminar.
+//  - Si se elige eliminar, realizar petición DELETE /api/tasks/[id]/
+// TODO Opción para salir/desactivar usuario:
+//  - Cambiar el valor de active a false: PUT /api/users/[id]/
+//  - Redirigir a la pantalla anterior window.location = 'index.html'
